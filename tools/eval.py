49f9016295583c3f3f907304c7d323c222d841b6
Qianqian0622 <49578146+Qianqian0622@users.noreply.github.com>
2020-03-14 21:07:27 -0500

    Add files via upload

--- tools/eval.py
+++ tools/eval.py
@@ -1,17 +1,60 @@
 # -*- coding:utf-8 -*-
+from __future__ import absolute_import, division, print_function
 
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
 
+import os
+from os.path import join as pjoin
+import numpy as np
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+
+import tensorflow as tf
+import tfplot as tf
+import tensorflow.contrib.slim as slim
 import os, sys
+import numpy as np
+import time
+sys.path.append("../")
+
+from libs.configs import cfgs
+#from libs.networks import build_whole_network2
+from libs.networks import build_whole_network
+from data.io.read_tfrecord import next_batch
+from libs.box_utils import show_box_in_tensor
+from help_utils import tools
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import libs
+import help_utils
+from libs import configs,networks
+from libs.networks import resnet
+from libs.networks import mobilenet_v2
+from libs.box_utils import encode_and_decode
+from libs.box_utils import boxes_utils
+from libs.box_utils import anchor_utils
+from libs.configs import cfgs
+from libs.losses import losses
+from libs.box_utils import show_box_in_tensor
+from libs.detection_oprations.proposal_opr import postprocess_rpn_proposals
+from libs.detection_oprations.anchor_target_layer_without_boxweight import anchor_target_layer
+from libs.detection_oprations.proposal_target_layer import proposal_target_layer
+
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.slim.nets import resnet_v1
+from tensorflow.contrib.slim.nets import resnet_utils
+from tensorflow.contrib.slim.python.slim.nets.resnet_v1 import resnet_v1_block
+
 import tensorflow as tf
 import time
 import cv2
 import pickle
-import numpy as np
-sys.path.append("../")
 
+sys.path.append("../")
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from data.io.image_preprocess import short_side_resize_for_inference_data
 from libs.configs import cfgs
 from libs.networks import build_whole_network
@@ -161,7 +204,7 @@ def parse_args():
                         default=False, type=bool)
     parser.add_argument('--GPU', dest='GPU',
                         help='gpu id',
-                        default='2', type=str)
+                        default='0', type=str)
     #parser.add_argument('--eval_num', dest='eval_num',
     #                    help='the num of eval imgs',
     #                    default=np.inf, type=int)
